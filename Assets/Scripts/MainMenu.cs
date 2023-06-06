c01f01e13d25905960876666bbcd145254fd1312
Max Manenschijn <maxmanenschijn@gmail.com>
2023-06-06 15:33:30 +0200

    Added fonts and UI elements

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour
    
     public void PlayButton() 
     {
-        SceneManager.LoadScene("OtherSceneName");
+        SceneManager.LoadScene(0);
     }
 
     public void QuiteButton() 
